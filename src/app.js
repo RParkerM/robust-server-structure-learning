3213f362caa2a7b7bf8daf3a127d61b1017e9788
Parker Mitchell <richard.parker.mitchell@gmail.com>
2021-06-19 23:18:27 -0700

    added functionality for adding flips

--- src/app.js
+++ src/app.js
@@ -5,6 +5,19 @@ const app = express();
 const flips = require("./data/flips-data");
 const counts = require("./data/counts-data");
 
+app.use(express.json());
+
+app.use("/counts/:countId", (req, res, next) => {
+  const { countId } = req.params;
+  const foundCount = counts[countId];
+
+  if (foundCount === undefined) {
+    next(`Count id not found: ${countId}`);
+  } else {
+    res.json({ data: foundCount }); // Return a JSON object, not a number.
+  }
+});
+
 app.use("/counts", (req, res) => {
   res.json({ data: counts });
 });
@@ -20,9 +33,27 @@ app.use("/flips/:flipId", (req, res, next) => {
   }
 });
 
-app.use("/flips", (req, res) => {
+app.get("/flips", (req, res) => {
   res.json({ data: flips });
 });
+
+let lastFlipId = flips.reduce((maxId, flip) => Math.max(maxId, flip.id), 0);
+
+app.post("/flips", (req, res, next) => {
+  const { data: { result } = {} } = req.body;
+  if (result) {
+    const newFlip = {
+      id: ++lastFlipId, // Increment last ID, then assign as the current ID
+      result,
+    };
+    flips.push(newFlip);
+    counts[result] = counts[result] + 1; // Increment the counts
+    res.status(201).json({ data: newFlip });
+  } else {
+    res.sendStatus(400);
+  }
+});
+
 // Not found handler
 app.use((request, response, next) => {
   next(`Not found: ${request.originalUrl}`);
