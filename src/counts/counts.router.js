6cc352b8c517d5e3cfac254d869e5215ebc8e5fb
Parker Mitchell <richard.parker.mitchell@gmail.com>
2021-06-24 22:29:46 -0700

    Clarified reasoning for using flipsRouter in countsRouter

--- src/counts/counts.router.js
+++ src/counts/counts.router.js
@@ -4,9 +4,11 @@ const methodNotAllowed = require("../errors/methodNotAllowed");
 
 const flipsRouter = require("../flips/flips.router");
 
+//Will display only the flips that are associated with the countId specified
 router.use("/:countId/flips", controller.countExists, flipsRouter);
 
 router.route("/:countId").get(controller.read).all(methodNotAllowed);
+
 router.route("/").get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
